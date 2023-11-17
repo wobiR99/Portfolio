ef983916ac7cb5114bcc49c60e9c6c47d743e74d
Obi ifeanyi <obiifeanyi.fi@gmail.com>
2023-11-17 20:29:05 +0100

    Error Corrections

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -26,7 +26,7 @@ const Ball = (props) => {
         <Decal
           map={decal}
           position={[0, 0, 1]}
-          rotation={(2 * Math.PI, 0, 6.25)}
+          rotation={[2 * Math.PI, 0, 6.25]}
           flatShading
         />
       </mesh>
